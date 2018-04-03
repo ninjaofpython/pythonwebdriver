2235808a237d12adf068800cf643b14cdae5a2f1
Gleg Oden <subxim@gmail.com>
2018-04-02 21:19:25 -0700

    Update FindbyClassNameandTagName.py

--- FindbyClassNameandTagName.py
+++ FindbyClassNameandTagName.py
@@ -6,18 +6,18 @@ class FindByClassNameandTagName():
    
     
     def test(self):
-        baseURL = "https://letskodeit.teachable.com/pages/practice"
+        baseURL = "https://www.python.com"
         driver = webdriver.Firefox()
         driver.get(baseURL)
         
         elementByClassName = driver.find_element_by_class_name("displayed-class")
-        elementByClassName.send_keys("Testing this thing 1")
+        elementByClassName.send_keys("Testing find the element.")
 
         if elementByClassName is not None:
             print("We found an element by ClassName")
 
         elementByTagName = driver.find_element_by_tag_name("h1")
-        elementByTagName.send_keys("Testing this thing")
+        elementByTagName.send_keys("Testing this with tag_name element.")
         if elementByTagName is not None:
             print("We found the element by Tag Element!")
 ff = FindByClassNameandTagName()
